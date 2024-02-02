42023de51a2746e1e853dec1d850d86220320b07
Alex2910 <alexlll12345lll@gmail.com>
2024-02-02 19:11:36 +0500

    first

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-// let days = document.querySelectorAll(".timer_value")[0]
-// let hours = document.querySelectorAll(".timer_value")[1]
-// let minuts = document.querySelectorAll(".timer_value")[2]
-// let seconds = document.querySelectorAll('.timer_value')[3]
+let days = document.querySelectorAll(".timer_value")[0]
+let hours = document.querySelectorAll(".timer_value")[1]
+let minuts = document.querySelectorAll(".timer_value")[2]
+let seconds = document.querySelectorAll('.timer_value')[3]
 
-// deadlineDate = new Date(2024,1,2,23,59,59)
+deadlineDate = new Date(2024,1,2,23,59,59)
 
-// function countDown() {
-//     let newDate= new Date()
-//     let resultDate = deadlineDate - newDate
-//         if (resultDate <= 0) {
+function countDown() {
+    let newDate= new Date()
+    let resultDate = deadlineDate - newDate
+        if (resultDate <= 0) {
+            clearInterval(timer)
+        }
+        else {
+            let daysValue = Math.floor(resultDate/(24*60*60*1000))
+            let hoursValue = Math.floor((resultDate % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000))
+            let minutsValue = Math.floor((resultDate%(60*60*1000))/(60*1000))
+            let secondsValue = Math.floor((resultDate%(60*1000))/1000)
+            hold(daysValue, days)
+            hold(hoursValue, hours)
+            hold(minutsValue, minuts)
+            hold(secondsValue, seconds)
+        }
+}
 
-//         }
-//         else {
-//             let daysValue = Math.floor(resultDate/(24*60*60*1000))
-//             let hoursValue = Math.floor((resultDate % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000))
-//             let minutsValue = Math.floor((resultDate%(60*60*1000))/(60*1000))
-//             let secondsValue = Math.floor((resultDate%(60*1000))/1000)
-//             days.innerHTML = daysValue
-//             hours.innerHTML = hoursValue
-//             minuts.innerHTML = minutsValue
-//             seconds.innerHTML = secondsValue
-//         }
-// }
+let timer = setInterval(countDown, 1000)
 
-// let timer = setInterval(countDown, 1000)
+function hold(elem, value) {
+    if(elem<10) {
+        value.innerHTML="0"+elem
+    }
+    else {
+        value.innerHTML=elem
+    }
+}
